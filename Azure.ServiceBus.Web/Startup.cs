2ee2f5a1a3ce74fbe5b3e7b3e691b42e2ab0d4cb
yunqian44 <yunqian8@live.com>
2021-01-02 23:58:49 +0800

    Add Dependency injection of Appsettings  Class，Service Class QueueClient

--- Azure.ServiceBus.Web/Startup.cs
+++ Azure.ServiceBus.Web/Startup.cs
@@ -1,7 +1,10 @@
+using Azure.ServiceBus.Web.Common;
+using Azure.ServiceBus.Web.Service;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using AzureServiceBus = Microsoft.Azure.ServiceBus;
 using Microsoft.Extensions.Hosting;
 using System;
 using System.Collections.Generic;
@@ -12,16 +15,27 @@ namespace Azure.ServiceBus.Web
 {
     public class Startup
     {
-        public Startup(IConfiguration configuration)
+        public Startup(IConfiguration configuration,IWebHostEnvironment env)
         {
+            Env = env;
             Configuration = configuration;
         }
 
         public IConfiguration Configuration { get; }
 
+        public IWebHostEnvironment Env { get; }
+
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddSingleton(new Appsettings(Env.ContentRootPath));
+
+            services.AddSingleton<AzureServiceBus.QueueClient>(new AzureServiceBus.QueueClient(Appsettings.app("ServiceBus", "PrimaryConnectionString"), Appsettings.app("ServiceBus", "QueueName")));
+
+            services.AddSingleton<IServiceBusQueueService, ServiceBusQueueService>();
+
+            
+
             services.AddControllersWithViews();
         }
 
