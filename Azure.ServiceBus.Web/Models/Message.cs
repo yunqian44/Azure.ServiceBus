b58d1be1024bb109662431197a3590784e8aeeed
yunqian44 <yunqian8@live.com>
2021-01-02 23:54:10 +0800

    Add Messgae Model

---
+++ Azure.ServiceBus.Web/Models/Message.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Azure.ServiceBus.Web.Models
+{
+    public class Message
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Content { get; set; }
+        public DateTime CreateTime { get; set; }
+    }
+}
